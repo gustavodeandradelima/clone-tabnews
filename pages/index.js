b9faf815a5617efe039442bd8e742f1f8c1e5378
gustavo_andrade <gustavo.andrade@sds.pe.gov.br>
2025-06-27 16:25:04 -0300

    atualizado

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>Teste Novo depois para emendar para mostrat o errosssssssssssssssssssssssssss já no vercel</h1>
+function Home() {
+  return (
+    <h1>
+      Teste Novo depois para emendar para mostrat o
+      errosssssssssssssssssssssssssss já no vercel
+    </h1>
+  );
 }
 
 export default Home;
